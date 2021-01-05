9302a291cd0496214dd3797a598e24ca80830eff
jeffubayi <jeffubayi@gmail.com>
2021-01-05 19:49:05 +0300

    add document/content

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -45,7 +45,7 @@ const Footer = ({
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Made by <a href="https://cruip.com">Cruip</a>. All right reserved</div>
+            <div className="footer-copyright">Made by <a href="https://jeffubayi.site">Jeff Ubayi</a>. All right reserved</div>
           </div>
         </div>
       </div>
